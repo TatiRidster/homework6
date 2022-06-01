751c08d937b9e7810de9e1ec37e109280c811a13
Tatiana <tatnovid@gmail.com>
2022-06-01 16:58:28 +0300

    сколько чисел больше 0 ввёл пользователь

---
+++ ex41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+
+int PositiveNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+Console.Write("Введите числа через запятую: ");
+int[] array = Console.ReadLine().Split(',').Select(x => int.Parse(x)).ToArray(); //Данный способ ввода массива с лекции про то, как писать код
+Console.WriteLine(PositiveNumbers(array));
